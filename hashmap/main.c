7ba7edcf18a34ac641bcc457b2f396aa73c34eb6
haruInDisguise <justhoro@protonmail.com>
2023-07-17 22:21:17 +0200

    [hashmap] In main.c: Cleanup and extended testing
    Removed some duplicate code and extended main with testing that should
    be easier to debug.

--- hashmap/main.c
+++ hashmap/main.c
@@ -1,125 +1,59 @@
-#include <stdint.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <assert.h>
 
-#define INITIAL_SIZE 48
-#define UPSIZE_AT_PERCENT 75
-#define DOWNSIZE_AT_PERCENT 50
+#include "hashmap.h"
 
-typedef uint32_t (*hm_hash_func)(char* value, uint32_t seed);
+#define NUM_ELEMENTS 32
+#define KEY_SIZE 16
 
-typedef struct {
-    uint8_t occupied;
-    void *value;
-    char *key;
-} hm_Bucket;
+static uint32_t string_hash_func(char *key, uint32_t seed) {
+    uint32_t hash = 0;
 
-typedef struct {
-    uint32_t total_capacity;
-    uint32_t used_capacity;
-    uint32_t seed_one;
-    uint32_t seed_two;
-
-    hm_hash_func hash_func;
-    hm_Bucket *buckets;
-} hm_HashMap;
-
-static uint32_t string_hash_func(char *value, uint32_t seed) {
-    uint32_t key = 0;
-
-    for(uint32_t i = 0; i < strlen(value); i++) {
-        key += (value[i] * seed + i);
-    }
-
-    return key;
-}
-
-static uint32_t get_hash(hm_HashMap *map, char *key, uint32_t attempt) {
-    uint32_t hash = map->hash_func(key, map->seed_one);
-
-    if(attempt > 0) {
-        hash = hash + attempt * (map->hash_func(key, map->seed_two) + 1);
+    for (uint32_t i = 0; i < strlen(key); i++) {
+        hash += (key[i] * seed + i);
     }
 
     return hash;
 }
 
-static uint32_t get_index(hm_HashMap *map, char *key) {
-    uint32_t attempt = 0;
-    uint32_t index = 0;
-
-    while(1) {
-        const uint32_t hash = get_hash(map, key, attempt);
-        index = hash % map->total_capacity;
-        if (map->buckets[index].occupied == false) {
-            break;
-        }
-
-        attempt ++;
-    }
-
-    printf("Merge conflict! Key: %s, Total attempts: %u\n", key, attempt);
-
-    return index;
-}
-
-static void rehash(hm_HashMap *map) {
-    // TODO
-}
-
-void hm_insert(hm_HashMap *map, char *key, void *value) {
-    uint32_t index = get_index(map, key);
-
-    if(map->buckets[index].occupied == true) {
-        fprintf(stderr, "Merge conflict! \"%s\"\n", key);
-        exit(1);
-    }
-
-    // TODO: Resize
-
-    map->buckets[index] = (hm_Bucket) {
-        .key = key,
-        .value = value,
-        .occupied = true,
-    };
-}
-
-void *hm_get(hm_HashMap *map, char *key) {
-    return map->buckets[get_index(map, key)].value;
-}
-
-hm_HashMap *hm_new(hm_hash_func hash_func, uint64_t seed_one, uint64_t seed_two) {
-    hm_HashMap *map = calloc(1, sizeof(*map));
-
-    *map = (hm_HashMap) {
-        .buckets = calloc(INITIAL_SIZE, sizeof(*map->buckets)),
-        .hash_func = hash_func,
-        .total_capacity = INITIAL_SIZE,
-        .seed_one = seed_one,
-        .seed_two = seed_two,
-    };
-
-    return map;
-}
-
-void hm_destroy(hm_HashMap *map) {
-    free(map->buckets);
-    free(map);
-}
-
-int main(int argc, char **argv) {
+#if 0
+int main(void) {
     hm_HashMap *map = hm_new(string_hash_func, 3242399, 324239941);
-    char buffer[32];
 
-    for(uint32_t i = 0; i < 32; i++) {
-        // NOTE: Look into __attribute__((format(...)))
-        snprintf(buffer, 32, "awesome_key_%u", i);
-        hm_insert(map, buffer, &i);
+    for(uint32_t index = 0; index < NUM_ELEMENTS; index ++) {
+        hm_insert(map, "some value", &(int){42});
     }
 
     hm_destroy(map);
 
     return 0;
 }
+
+#else
+// avoding bitfiels?: https://stackoverflow.com/questions/46021790/are-there-reasons-to-avoid-bit-field-structure-members
+int main(int argc, char **argv) {
+    struct {
+        char buffer[KEY_SIZE];
+        uint32_t value;
+    } *pairs = calloc(NUM_ELEMENTS, sizeof(*pairs));
+
+    hm_HashMap *map = hm_new(string_hash_func, 3242399, 324239941);
+    uint32_t index = 0;
+
+    for (index = 0; index < NUM_ELEMENTS; index++) {
+        // NOTE: Look into __attribute__((format(...)))
+        snprintf(pairs[index].buffer, NUM_ELEMENTS, "awesome_key_%u", index);
+        hm_insert(map, pairs[index].buffer, &pairs[index].value);
+        pairs[index].value = index;
+    }
+
+    for (index = 0; index < NUM_ELEMENTS; index++) {
+        uint32_t *value = hm_get(map, pairs[index].buffer);
+        assert(*value == index);
+    }
+
+    hm_destroy(map);
+    free(pairs);
+
+    return 0;
+}
+#endif
